6b68a0b25aecb516d4f77b3cec7c9a9c14451094
Brian Yan <score14@gmail.com>
2016-04-22 23:13:22 -0700

    Initial Commit

---
+++ JustShipIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustShipIt
+//
+//  Created by Brian Yan on 4/22/16.
+//  Copyright © 2016 Brian Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

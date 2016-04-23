0b7f8337d0e9be7e5b883ca0f23ab29b6a3a3310
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-04-23 16:17:28 -0700

    Added constraints to home and JustShipIt views

--- JustShipIt/ViewController.swift
+++ JustShipIt/ViewController.swift
@@ -9,7 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var width: UITextField!
+    @IBOutlet weak var length: UITextField!
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var weight: UITextField!
+    @IBOutlet weak var shippingDestination: UITextField!
 
+    func isStringEmpty(stringValue:String) -> Bool
+    {
+        var returnValue = false
+        
+        // Make sure user did not submit number of empty spaces
+        let stringNoSpaces = stringValue.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        
+        if(stringNoSpaces.isEmpty == true)
+        {
+            returnValue = true
+            return returnValue
+        }
+        
+        return returnValue
+        
+    }
+    
+    @IBAction func justShipIt(sender: AnyObject) {
+        let widthValue = width.text
+        let lengthValue = length.text
+        let heightValue = height.text
+        let weightValue = weight.text
+        let shippingDestinationValue = shippingDestination.text
+        
+        // Check is textboxes are empty
+        if isStringEmpty(widthValue!) == true
+        {
+            return
+        }
+        
+        // If not null, then do http request
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

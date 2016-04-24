76614c0caf3b9966ecdc07c6cd91ebdd5917b155
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-04-23 18:44:38 -0700

    Added Alamofire library using cocoapods for http post requests

--- JustShipIt/ViewController.swift
+++ JustShipIt/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var width: UITextField!
@@ -14,23 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var height: UITextField!
     @IBOutlet weak var weight: UITextField!
     @IBOutlet weak var shippingDestination: UITextField!
-
-    func isStringEmpty(stringValue:String) -> Bool
-    {
-        var returnValue = false
-        
-        // Make sure user did not submit number of empty spaces
-        let stringNoSpaces = stringValue.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-        
-        if(stringNoSpaces.isEmpty == true)
-        {
-            returnValue = true
-            return returnValue
-        }
-        
-        return returnValue
-        
-    }
+    @IBOutlet weak var item: UITextField!
     
     @IBAction func justShipIt(sender: AnyObject) {
         let widthValue = width.text
@@ -38,14 +23,16 @@ class ViewController: UIViewController {
         let heightValue = height.text
         let weightValue = weight.text
         let shippingDestinationValue = shippingDestination.text
+        let itemValue = item.text
+        let itemDouble = Double(itemValue!)
         
-        // Check is textboxes are empty
-        if isStringEmpty(widthValue!) == true
-        {
-            return
-        }
+        let myURL = NSURL(string: "https://sandbox.shiphawk.com/api/v4/rates?api_key=ce664be73291da576420dc85e005ef1d");
+        let request = NSMutableURLRequest(URL:myURL!);
+        request.HTTPMethod = "POST";
+        let postParameters = ["length", ] as Dictionary<String, String>;
+        request.HTTPBody = postParameters.dataUsingEncoding(NSUTF8StringEncoding);
         
-        // If not null, then do http request
+        
         
     }
     

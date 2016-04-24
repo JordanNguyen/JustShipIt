e21b0ceeb765d1faefc7aeaf531bde7830d14446
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-04-23 19:52:12 -0700

    Performed a working GET request using Shiphawk API

--- JustShipIt/ViewController.swift
+++ JustShipIt/ViewController.swift
@@ -49,38 +49,32 @@ class ViewController: UIViewController {
 //            ]
 //        ]
 //        
-//        Alamofire.request(.POST, "https://sandbox.shiphawk.com/api/v4/rates?api_key=ce664be73291da576420dc85e005ef1d", parameters: parameters, encoding: .JSON).responseJSON
-//            { response in switch response.result {
-//            case .Success(let JSON):
-//                print("Success with JSON: \(JSON)")
-//                
-//                let response = JSON as! NSDictionary
-//                
-//                //example if there is an id
-////                let userId = response.objectForKey("id")!
-//                
-//            case .Failure(let error):
-//                print("Request failed with error: \(error)")
-//                }
-//        }
-        
-        Alamofire.request(.GET, "https://sandbox.shiphawk.com/api/v4/unpacked_item_types/search?name=chair&api_key=ce664be73291da576420dc85e005ef1d")
-//            .response { request, response, data, error in
-//                print(request)
-//                print(response)
-//                print(data!)
-////                print(error)
-//        }
-            .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
+        Alamofire.request(.POST, "https://sandbox.shiphawk.com/api/v4/rates?api_key=ce664be73291da576420dc85e005ef1d", parameters: parameters, encoding: .JSON).responseJSON
+            { response in switch response.result {
+            case .Success(let JSON):
+                print("Success with JSON: \(JSON)")
                 
-                if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
+                let response = JSON as! NSDictionary
+                
+                //example if there is an id
+//                let userId = response.objectForKey("id")!
+                
+            case .Failure(let error):
+                print("Request failed with error: \(error)")
                 }
         }
+        
+//        Alamofire.request(.GET, "https://sandbox.shiphawk.com/api/v4/unpacked_item_types/search?name=chair&api_key=ce664be73291da576420dc85e005ef1d")
+//            .responseJSON { response in
+//                print(response.request)  // original URL request
+//                print(response.response) // URL response
+//                print(response.data)     // server data
+//                print(response.result)   // result of response serialization
+//                
+//                if let JSON = response.result.value {
+//                    print("JSON: \(JSON)")
+//                }
+//        }
     }
     
     override func viewDidLoad() {

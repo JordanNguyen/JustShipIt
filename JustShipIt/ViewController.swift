1af55ae1c42060af1ab0b2404e37079d4a6986b1
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-04-23 20:57:30 -0700

    Fixed textbox errors

--- JustShipIt/ViewController.swift
+++ JustShipIt/ViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-    @IBOutlet weak var width: UITextField!
-    @IBOutlet weak var length: UITextField!
-    @IBOutlet weak var height: UITextField!
-    @IBOutlet weak var weight: UITextField!
-    @IBOutlet weak var shippingDestination: UITextField!
-    @IBOutlet weak var item: UITextField!
+    @IBOutlet var width: UITextField!
+
+    @IBOutlet var length: UITextField!
     
+    @IBOutlet var height: UITextField!
+    
+    @IBOutlet var weight: UITextField!
+    
+    @IBOutlet var shippingDestination: UITextField!
+    
+    @IBOutlet var item: UITextField!
     @IBAction func justShipIt(sender: AnyObject) {
         let widthValue = width.text
         let lengthValue = length.text
@@ -39,14 +43,7 @@ class ViewController: UIViewController {
             "origin_address": ["zip": "93101"],
             "destination_address": ["zip": shippingDestinationValue!]
         ]
-
-//        let parameters = [
-//            "foo": [1,2,3],
-//            "bar": [
-//                "baz": "qux"
-//            ]
-//        ]
-//        
+        
         Alamofire.request(.POST, "https://sandbox.shiphawk.com/api/v4/rates?api_key=ce664be73291da576420dc85e005ef1d", parameters: parameters, headers: ["Content-Type": "applications/json"], encoding: .JSON).responseJSON
             { response in switch response.result {
             case .Success(let JSON):
@@ -54,25 +51,10 @@ class ViewController: UIViewController {
                 
                 let response = JSON as! NSDictionary
                 
-                //example if there is an id
-//                let userId = response.objectForKey("id")!
-                
             case .Failure(let error):
                 print("Request failed with error: \(error)")
                 }
         }
-        
-//        Alamofire.request(.GET, "https://sandbox.shiphawk.com/api/v4/unpacked_item_types/search?name=chair&api_key=ce664be73291da576420dc85e005ef1d")
-//            .responseJSON { response in
-//                print(response.request)  // original URL request
-//                print(response.response) // URL response
-//                print(response.data)     // server data
-//                print(response.result)   // result of response serialization
-//                
-//                if let JSON = response.result.value {
-//                    print("JSON: \(JSON)")
-//                }
-//        }
     }
     
     override func viewDidLoad() {

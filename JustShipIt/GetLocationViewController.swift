ae0e74d54cdb5e0e65e81d5443b70523a7aa0490
Brian Yan <score14@gmail.com>
2016-04-23 17:15:33 -0700

    added items

---
+++ JustShipIt/GetLocationViewController.swift
@@ -0,0 +1,62 @@
+//
+//  GetLocationViewController.swift
+//  JustShipIt
+//
+//  Created by Brian Yan on 4/23/16.
+//  Copyright © 2016 Brian Yan. All rights reserved.
+//
+import MapKit
+import UIKit
+import CoreLocation
+
+class GetLocationViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    @IBOutlet var mapview: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapview.showsUserLocation = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapview.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError)
+    {
+        print("Errors: " + error.localizedDescription)
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JustShipIt/getLocation.swift
@@ -0,0 +1,9 @@
+//
+//  getLocation.swift
+//  JustShipIt
+//
+//  Created by Brian Yan on 4/23/16.
+//  Copyright © 2016 Brian Yan. All rights reserved.
+//
+
+import Foundation
